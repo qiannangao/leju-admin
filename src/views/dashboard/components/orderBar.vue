90d43d3289a4a556c91111935d7aaa8a02fa2d32
高倩南 <3065966250@qq.com>
2023-08-14 22:23:07 +0800

    four

--- src/views/dashboard/components/orderBar.vue
+++ src/views/dashboard/components/orderBar.vue
@@ -24,21 +24,57 @@ export default {
       const barEchart = echarts.init(document.getElementById('order-bar'))
       const option = {
         title: {
-          text: '订单退单柱状图'
+          text: '订单退单柱状图',
+          textStyle: {
+            color: '#94A2CD',
+            fontStyle: 'normal'
+          }
         },
-        tooltip: {},
+        tooltip: {
+          backgroundColor: 'rgba(0,0,0,0.5)',
+          formatter: function(params, ticket, callback) {
+            return `${params.seriesName}<br/>${params.marker}${params.name}:${params.value}`
+          },
+          textStyle: {
+            color: '#fff'
+          }
+        },
+        legend: {
+          type: 'plain',
+          bottom: 10
+        },
         xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
         },
-        yAxis: {},
+        yAxis: {
+          type: 'value',
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: '#979AA5'
+            }
+          },
+          axisTick: {
+            show: true
+          }
+        },
         series: [
           {
-            name: '销量',
+            name: '订单',
+            data: [120, 200, 150, 80, 70, 110, 130],
             type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
+            color: ['#AD01F9']
+          },
+          {
+            name: '退单',
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar',
+            color: ['#23AEF4']
           }
         ]
       }
+
       barEchart.setOption(option)
     }
   }
